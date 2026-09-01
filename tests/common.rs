f5588c3c63f92f05b51f55eae6702d362207d3fd
Wim Velzeboer <wim@velzeboer.nl>
2026-09-01 13:34:51 +0100

    FMT formatting tests

--- tests/common.rs
+++ tests/common.rs
@@ -1,25 +1,25 @@
-use rustsf::{Client, RestApi, DefSObject, AuthUrl};
-use std::env;
 use anyhow::Result;
+use rustsf::{AuthUrl, Client, DefSObject, RestApi};
+use std::env;
 
 pub async fn get_rest_api_client() -> Result<RestApi> {
-    let url = env::var("SCRATCH_AUTH_URL").expect("SCRATCH_AUTH_URL not set").to_string();
-    let client = Client::new(AuthUrl::new(url)?).await?;
-    Ok(RestApi::new(client))
+	let url = env::var("SCRATCH_AUTH_URL")
+		.expect("SCRATCH_AUTH_URL not set")
+		.to_string();
+	let client = Client::new(AuthUrl::new(url)?).await?;
+	Ok(RestApi::new(client))
 }
 
-#[DefSObject(sobject_type = "Account", fields="name,owner")]
-pub struct Account {
-
-}
+#[DefSObject(sobject_type = "Account", fields = "name,owner")]
+pub struct Account {}
 
 impl Account {
-    pub fn set_name(mut self, name: String) -> Self {
-        self.name = Some(name);
-        self
-    }
+	pub fn set_name(mut self, name: String) -> Self {
+		self.name = Some(name);
+		self
+	}
 
-    pub fn get_name(&self) -> Option<&str> {
-        self.name.as_deref()
-    }
+	pub fn get_name(&self) -> Option<&str> {
+		self.name.as_deref()
+	}
 }
--- tests/rest_api_integration_test.rs
+++ tests/rest_api_integration_test.rs
@@ -3,43 +3,48 @@ use anyhow::Result;
 
 mod common;
 
-
 ///  Check the available API versions
 #[tokio::test]
 #[ignore]
 async fn check_versions() -> Result<()> {
-    let mut client = common::get_rest_api_client().await?;
-    let versions = client.api_versions().await?;
+	let mut client = common::get_rest_api_client().await?;
+	let versions = client.api_versions().await?;
 
-    assert_ne!(0, versions.len());
-    assert_eq!("67.0", versions[versions.len()-1].version); // this forces us to always use the latest version
-    Ok(())
+	assert_ne!(0, versions.len());
+	assert_eq!("67.0", versions[versions.len() - 1].version); // this forces us to always use the latest version
+	Ok(())
 }
 
 /// Create an Account record and Fetch it via Id
 #[tokio::test]
 #[ignore]
 async fn create_fetch_delete_sobject() -> Result<()> {
-    let mut client = common::get_rest_api_client().await?;
+	let mut client = common::get_rest_api_client().await?;
 
-    // Create an Account record
-    let account = client.create_owned_sobject(common::Account::new()
-        .set_name("Test Account".to_string())).await?;
-    assert!(account.id.is_some());
-    assert_eq!(18, account.id.as_ref().unwrap().len()); // We got a 18 character Salesforce Id
+	// Create an Account record
+	let account = client
+		.create_owned_sobject(common::Account::new().set_name("Test Account".to_string()))
+		.await?;
+	assert!(account.id.is_some());
+	assert_eq!(18, account.id.as_ref().unwrap().len()); // We got a 18 character Salesforce Id
 
-    // Fetch the Account record
-    let account = client.fetch_by_id::<common::Account>("Account", account.id.as_ref().unwrap()).await?;
-    assert_eq!("Test Account", account.get_name().unwrap());
+	// Fetch the Account record
+	let account = client
+		.fetch_by_id::<common::Account>("Account", account.id.as_ref().unwrap())
+		.await?;
+	assert_eq!("Test Account", account.get_name().unwrap());
 
-    client.delete_sobject("Account", &account.id.clone().unwrap()).await?;
+	client.delete_sobject("Account", &account.id.clone().unwrap()).await?;
 
-    match client.fetch_by_id::<common::Account>("Account", account.id.as_ref().unwrap()).await {
-        Ok(_) => panic!("Account record should have been deleted"),
-        Err(_) => (),  // fixme assert the right error message
-    };
+	match client
+		.fetch_by_id::<common::Account>("Account", account.id.as_ref().unwrap())
+		.await
+	{
+		Ok(_) => panic!("Account record should have been deleted"),
+		Err(_) => (), // fixme assert the right error message
+	};
 
-    Ok(())
+	Ok(())
 }
 /*
 /// Fetch the Account record via SOQL
